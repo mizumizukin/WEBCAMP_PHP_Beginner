39edcf3ffda175addf88a3f2a27f74a65ab27ac2
MIZUKI NOJIMA <MIZUKI@example.com>
2024-07-17 13:39:23 +0000

    bit_operation.phpを書いた

---
+++ bit_operation.php
@@ -0,0 +1,18 @@
+<?php
+$i = random_int(0, 255);
+$j = random_int(0, 255);
+
+$k = $i & $j;
+printf("%x & %x = %x \n", $i, $j, $k);
+
+// OR
+$k = $i | $j;
+printf("%x | %x = %x \n", $i, $j, $k);
+
+// XOR
+$k = $i ^ $j;
+printf("%x ^ %x = %x \n", $i, $j, $k);
+
+// NOT
+$k = ~$i;
+printf("~%x = %x \n", $i, $k);
