ff3c5da2caff85111f3c832585cd7d11547a92ee
MIZUKI NOJIMA <MIZUKI@example.com>
2024-07-26 12:57:29 +0000

    switch.phpを書いた

---
+++ switch.php
@@ -0,0 +1,16 @@
+<?php
+//
+$i = 1;
+switch($i) {
+    case 0:
+        echo "iは０です \n";
+        break;
+
+    case 1 :
+        echo "iは１です \n";
+        break;
+
+    default:
+        echo "i は 0と1 以外です\n";
+        break;
+}
